682809d0454eaac04814bfa9400e7e5a65f2409b
致远520 <2296005140@qq.com>
2018-04-04 21:20:47 +0800

    简单点击按钮和简单交互

--- app/src/main/java/com/example/fds/myapplication/MainActivity.java
+++ app/src/main/java/com/example/fds/myapplication/MainActivity.java
@@ -1,13 +1,41 @@
 package com.example.fds.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
+import android.view.View.OnClickListener;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
+
+    private Button button;
+
+    private Integer count = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = (TextView)findViewById(R.id.textView);
+        button = (Button)findViewById(R.id.button);
+
+        ButtonListener buttonListener = new ButtonListener();
+        button.setOnClickListener(buttonListener);
+    }
+
+    /**
+     * 内部类
+     */
+    class ButtonListener implements OnClickListener {
+
+        @Override
+        public void onClick(View v) {
+            count++;
+            textView.setText(count+"");
+        }
     }
 }
