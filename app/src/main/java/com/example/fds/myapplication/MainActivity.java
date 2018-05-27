cdc67613c0469c36b8ae3ee6578a0fc40239b008
大双mmd <2296005140@qq.com>
2018-05-27 17:40:51 +0800

    简单点击按钮和简单交互/switch选择按钮demo

--- app/src/main/java/com/example/fds/myapplication/MainActivity.java
+++ app/src/main/java/com/example/fds/myapplication/MainActivity.java
@@ -5,7 +5,10 @@ import android.view.View.OnClickListener;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,17 +18,44 @@ public class MainActivity extends AppCompatActivity {
 
     private Integer count = 0;
 
+    private Switch aSwitch;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        textView = (TextView)findViewById(R.id.textView);
-        button = (Button)findViewById(R.id.button);
+        textView = findViewById(R.id.textView);
+        button = findViewById(R.id.button);
+        aSwitch = findViewById(R.id.switch1);
 
         ButtonListener buttonListener = new ButtonListener();
+        aSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener(){
+
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                switch (compoundButton.getId()){
+                    case R.id.switch1:
+                        if(compoundButton.isChecked()) Toast.makeText(MainActivity.this,"匿名内部类开关:ON",Toast.LENGTH_SHORT).show();
+                        else Toast.makeText(MainActivity.this,"匿名内部类开关:OFF",Toast.LENGTH_SHORT).show();
+                        break;
+
+                }
+            }
+        });
         button.setOnClickListener(buttonListener);
     }
+
+    /*@Override
+    public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+        switch (compoundButton.getId()){
+            case R.id.switch1:
+                if(compoundButton.isChecked()) Toast.makeText(this,"开关:ON",Toast.LENGTH_SHORT).show();
+                else Toast.makeText(this,"开关:OFF",Toast.LENGTH_SHORT).show();
+                break;
+
+        }
+    }*/
 
     /**
      * 内部类
@@ -35,7 +65,7 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void onClick(View v) {
             count++;
-            textView.setText(count+"");
+            textView.setText("count：" + count);
         }
     }
 }
